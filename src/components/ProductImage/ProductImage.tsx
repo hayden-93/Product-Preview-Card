84e3d0a82bf2b65cf014ed6874cc8925e832b667
Hayden Buckley-Smith <hayden@abstack.co>
2022-07-21 15:16:01 +0100

    create ProductImage component

---
+++ src/components/ProductImage/ProductImage.tsx
@@ -0,0 +1,11 @@
+interface ProductImageProps {
+  className?: string;
+  src?: string;
+  alt?: string;
+}
+
+export const ProductImage = ({ alt, className, src }: ProductImageProps) => {
+  const classes = ["", className].join(" ").trim();
+
+  return <img src={src} alt={alt} className={classes} />;
+};
---
+++ src/components/index.tsx
@@ -0,0 +1,6 @@
+export * from "./Button";
+export * from "./Category";
+export * from "./Description";
+export * from "./Price";
+export * from "./ProductImage";
+export * from "./Title";
